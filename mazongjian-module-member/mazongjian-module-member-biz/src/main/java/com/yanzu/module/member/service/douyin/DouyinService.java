2f3d2525ff55a0e1ae548d6ca9cbf5aac3e87695
DESKTOP-RBHMFJD\Administrator <mrguan@aliyun.com>
2024-08-07 15:14:02 +0800

    2024年8月7日 1.【升级】升级蓝牙门锁的插件版本 2.【修复】小程序门店管理-门店大门密码锁无法设置密码的问题 3.【优化】删除了新用户赠送加时券的功能，将在优化后再开放 4.【修复】自己申请抖音开发者平台时可能出现的验券问题 5.【修复】可能出现的优惠券已过期但是还能使用的问题

--- mazongjian-module-member/mazongjian-module-member-biz/src/main/java/com/yanzu/module/member/service/douyin/DouyinService.java
+++ mazongjian-module-member/mazongjian-module-member-biz/src/main/java/com/yanzu/module/member/service/douyin/DouyinService.java
@@ -160,7 +160,6 @@ public class DouyinService {
             verify_id = verify_results.getString("verify_id");
             certificate_id = verify_results.getString("certificate_id");
             return verify_id + "&" + certificate_id;
-
         } else if (data.getInteger("error_code") == 1228) {
             throw exception(GROUP_NO_CHECK_STORE_ERROR);
         } else {
