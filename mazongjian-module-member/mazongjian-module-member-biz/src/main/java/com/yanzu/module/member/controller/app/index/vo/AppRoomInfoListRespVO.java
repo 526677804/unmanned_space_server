1e68fd06a6405666e79ead8c293a25293eb28c47
XxxJoyFaculty <1792073177@qq.com>
2024-11-07 17:49:00 +0800

    +*：房间信息返回预定时间

--- mazongjian-module-member/mazongjian-module-member-biz/src/main/java/com/yanzu/module/member/controller/app/index/vo/AppRoomInfoListRespVO.java
+++ mazongjian-module-member/mazongjian-module-member-biz/src/main/java/com/yanzu/module/member/controller/app/index/vo/AppRoomInfoListRespVO.java
@@ -101,4 +101,8 @@ public class AppRoomInfoListRespVO {
     private Integer clearTime;
 
 
+    @Schema(description = "已被预定订单时间列表")
+    private List<AppOrderTimeVO> orderTimeList;
+
+
 }
--- mazongjian-module-member/mazongjian-module-member-biz/src/main/java/com/yanzu/module/member/service/index/IndexServiceImpl.java
+++ mazongjian-module-member/mazongjian-module-member-biz/src/main/java/com/yanzu/module/member/service/index/IndexServiceImpl.java
@@ -254,10 +254,15 @@ public class IndexServiceImpl implements IndexService {
         List<AppOrderTimeVO> bookings = new ArrayList<>();
         //找出该房间所有订单
         if (orederMap.containsKey(respVO.getRoomId())) {
-            List<OrderInfoDO> sortOrder = orederMap.get(respVO.getRoomId()).stream().sorted(Comparator.comparing(OrderInfoDO::getStartTime)).collect(Collectors.toList());
+            List<OrderInfoDO> sortOrder = orederMap.get(respVO.getRoomId())
+                    .stream().sorted(Comparator.comparing(OrderInfoDO::getStartTime))
+                    .collect(Collectors.toList());
+            List<AppOrderTimeVO> orderTimeVOList=new ArrayList<>(sortOrder.size());
             sortOrder.forEach(x -> {
                 bookings.add(new AppOrderTimeVO(x.getStartTime(), x.getEndTime()));
+                orderTimeVOList.add(new AppOrderTimeVO(x.getStartTime(),x.getEndTime()));
             });
+            respVO.setOrderTimeList(orderTimeVOList);
         }
         //每日不可用时间
         LocalTime bstart = null;
