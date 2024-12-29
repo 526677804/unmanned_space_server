e9f8ece4ffe3b2399fba8283bad30b6247d82a85
guanzhiqiang@aliyun.com <guanzhiqiang@aliyun.com>
2024-12-29 14:45:14 +0800

    fix:超级管理员 可以查看所有数据

--- mazongjian-framework/mazongjian-spring-boot-starter-biz-tenant/src/main/java/com/yanzu/framework/tenant/core/db/TenantDatabaseInterceptor.java
+++ mazongjian-framework/mazongjian-spring-boot-starter-biz-tenant/src/main/java/com/yanzu/framework/tenant/core/db/TenantDatabaseInterceptor.java
@@ -31,13 +31,16 @@ public class TenantDatabaseInterceptor implements TenantLineHandler {
 
     @Override
     public Expression getTenantId() {
+        Long requiredTenantId = TenantContextHolder.getRequiredTenantId();
         return new LongValue(TenantContextHolder.getRequiredTenantId());
     }
 
     @Override
     public boolean ignoreTable(String tableName) {
+        Long requiredTenantId = TenantContextHolder.getRequiredTenantId();
         return TenantContextHolder.isIgnore() // 情况一，全局忽略多租户
-            || CollUtil.contains(ignoreTables, tableName); // 情况二，忽略多租户的表
+                || CollUtil.contains(ignoreTables, tableName)// 情况二，忽略多租户的表
+                || requiredTenantId.intValue() == 1;// 情况三，超管忽略租户
     }
 
 }
